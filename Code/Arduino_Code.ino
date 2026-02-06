8b669f9b8e218cc7d7f3eb09a239f8e30306e2bb
Tripura-sorapalli <161053944+Tripura-sorapalli@users.noreply.github.com>
2026-02-06 19:13:16 +0530

    Update Arduino_Code.ino

--- Code/Arduino_Code.ino
+++ Code/Arduino_Code.ino
@@ -1,1 +1,43 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define SOIL_PIN A0
+#define LDR_PIN A1
+#define RELAY_PIN 7
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+  dht.begin();
+}
+
+void loop() {
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  int soilValue = analogRead(SOIL_PIN);
+  int ldrValue = analogRead(LDR_PIN);
+
+  int soilMoisture = map(soilValue, 1023, 0, 0, 100);
+
+  if (soilMoisture < 30) {
+    digitalWrite(RELAY_PIN, LOW);
+  } else {
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+
+  Serial.print(temperature);
+  Serial.print(",");
+  Serial.print(humidity);
+  Serial.print(",");
+  Serial.print(soilMoisture);
+  Serial.print(",");
+  Serial.println(ldrValue);
+
+  delay(2000);
+}
 
